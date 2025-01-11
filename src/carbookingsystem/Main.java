c9b39a3f425409da6c1db3cc71841ca4d4cb06fb
Medom05 <145607268+Medom5@users.noreply.github.com>
2025-01-11 21:21:40 +0200

    ADD: user class

--- src/carbookingsystem/Main.java
+++ src/carbookingsystem/Main.java
@@ -1,12 +1,14 @@
 package carbookingsystem;
 import carbookingsystem.car.Brand;
 import carbookingsystem.car.Car;
+import carbookingsystem.user.User;
 
 import java.math.BigDecimal;
+import java.util.UUID;
 
 public class Main {
     public static void main(String[] args) {
-        Car car = new Car(2,false, new BigDecimal(3), Brand.BMW);
-        System.out.println(car);
+        User user = new User(UUID.randomUUID(), "asdf");
+        System.out.println(user);
     }
 }
