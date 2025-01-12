27d0d25de05c85ec18dd9f44fb6f76ee80ab9d60
Medom05 <145607268+Medom5@users.noreply.github.com>
2025-01-12 14:43:05 +0200

    ADD: sample data for testing

--- src/carbookingsystem/car/CarDAO.java
+++ src/carbookingsystem/car/CarDAO.java
@@ -1,5 +1,6 @@
 package carbookingsystem.car;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,6 +9,9 @@ public class CarDAO {
 
     static {
         cars = new ArrayList<>();
+        cars.add(new Car("1234",  new BigDecimal("88.00"), Brand.VOLKSWAGEN, false));
+        cars.add(new Car("5678", new BigDecimal("60.00"), Brand.TESLA, true));
+        cars.add(new Car("4567", new BigDecimal("90.00"), Brand.BMW, false));
     }
 
     public List<Car> getAllCars() {
