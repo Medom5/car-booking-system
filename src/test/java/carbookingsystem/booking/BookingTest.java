c8aa03a7e2c875a6e8ab1a261079ed58b2365709
Medom05 <145607268+Medom5@users.noreply.github.com>
2025-01-17 20:43:07 +0200

    ADD: Booking model unit testing

---
+++ src/test/java/carbookingsystem/booking/BookingTest.java
@@ -0,0 +1,87 @@
+package carbookingsystem.booking;
+
+import carbookingsystem.car.Brand;
+import carbookingsystem.car.Car;
+import carbookingsystem.user.User;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class BookingTest {
+
+    @Test
+    void testBookingConstructorAndGetter() {
+        Car car = new Car("1234", new BigDecimal("88.00"), Brand.TESLA, true);
+        User user = new User(UUID.randomUUID(), "Peter");
+        UUID uuid = UUID.randomUUID();
+        LocalDateTime now = LocalDateTime.now();
+        Booking booking = new Booking(uuid, car, user, now);
+
+        assertEquals(uuid, booking.getBookingId());
+        assertEquals(car, booking.getCar());
+        assertEquals(user, booking.getUser());
+        assertEquals(now, booking.getBookingTime());
+    }
+
+    @Test
+    void testBookingSetter() {
+        Car car = new Car("1234", new BigDecimal("88.00"), Brand.TESLA, true);
+        User user = new User(UUID.randomUUID(), "Peter");
+        UUID uuid = UUID.randomUUID();
+        LocalDateTime now = LocalDateTime.now();
+        Booking booking = new Booking(uuid, car, user, now);
+
+        booking.setBookingId(UUID.randomUUID());
+        booking.setCar(new Car("5678", new BigDecimal("83.00"), Brand.HONDA, false));
+        booking.setUser(new User(UUID.randomUUID(), "Roland"));
+        booking.setBookingTime(LocalDateTime.now());
+        booking.setCancelled(true);
+
+        assertEquals(uuid, booking.getBookingId());
+        assertEquals(car, booking.getCar());
+        assertEquals(user, booking.getUser());
+        assertEquals(now, booking.getBookingTime());
+        assertFalse(booking.isCancelled());
+    }
+
+    @Test
+    void testBookingEqualsAndHashCode() {
+        UUID userId = UUID.randomUUID();
+        Car car = new Car("1234", new BigDecimal("88.00"), Brand.TESLA, true);
+        User user = new User(userId, "Peter");
+        UUID uuid = UUID.randomUUID();
+        LocalDateTime now = LocalDateTime.now();
+        Booking booking = new Booking(uuid, car, user, now);
+
+        Car car2 = new Car("1234", new BigDecimal("88.00"), Brand.TESLA, true);
+        User user2 = new User(userId, "Peter");
+        Booking booking2 = new Booking(uuid, car2, user2, now);
+
+        assertEquals(booking, booking2);
+        assertEquals(booking.hashCode(), booking2.hashCode());
+    }
+
+    @Test
+    void testBookingToString() {
+            Car car = new Car("1234", new BigDecimal("88.00"), Brand.TESLA, true);
+            User user = new User(UUID.randomUUID(), "Peter");
+            UUID uuid = UUID.randomUUID();
+            LocalDateTime now = LocalDateTime.now();
+
+            Booking booking = new Booking(uuid, car, user, now);
+
+            String expectedToString = "Booking{" +
+                    "id=" + uuid +
+                    ", car=" + car +
+                    ", user=" + user +
+                    ", bookingTime=" + now +
+                    ", isCancelled=false}";
+
+            assertEquals(expectedToString, booking.toString());
+
+    }
+}
