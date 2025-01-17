b5056529a93bc1651a551356b78506f168866a2a
Medom05 <145607268+Medom5@users.noreply.github.com>
2025-01-18 00:05:14 +0200

    FIX: data override when using java faker on each get user call

--- src/main/java/carbookingsystem/user/UserService.java
+++ src/main/java/carbookingsystem/user/UserService.java
@@ -4,18 +4,18 @@ import java.util.List;
 import java.util.UUID;
 
 public class UserService {
-    private final UserDao userDao;
+    private final List<User> allUsers;
 
     public UserService(UserDao userDao) {
-        this.userDao =  userDao;
+        this.allUsers = userDao.getAllUsers(); // replaced  userDao with list to keep java faker random result and not  override users on each call
     }
 
     public List<User> getAllUsers() {
-        return userDao.getAllUsers();
+        return allUsers;
     }
 
     public User getUserById(UUID id) {
-        return getAllUsers().stream()
+        return allUsers.stream()
                 .filter(user -> user.getId().equals(id))
                 .findFirst()
                 .orElse(null);
